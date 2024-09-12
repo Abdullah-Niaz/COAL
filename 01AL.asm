7959e2ac3b1bd1f28f7e06045ea59c11ec373075
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-09-12 22:45:24 +0500

    addition

--- 01AL.asm
+++ 01AL.asm
@@ -1,12 +1,12 @@
-org 0x0100            ; Origin, set the starting address
+org  0x0100              ; Origin, set the starting address
  
-mov ax, 5             ; Move the constant 5 into register AX
-mov bx, 10            ; Move the constant 10 into register BX
+mov  ax,       5         ; Move the constant 5 into register AX
+mov  bx,       10        ; Move the constant 10 into register BX
 
-add ax, bx            ; Add the value in BX to AX (AX = AX + BX)
+add  ax,       bx        ; Add the value in BX to AX (AX = AX + BX)
 
-mov bx, 15            ; Move the constant 15 into register BX
-add ax, bx            ; Add the value in BX to AX (AX = AX + BX)
+mov  bx,       15        ; Move the constant 15 into register BX
+add  ax,       bx        ; Add the value in BX to AX (AX = AX + BX)
 
-mov ax, 0x4c00        ; Terminate program
-int 0x21              ; DOS interrupt to exit
+mov  ax,       0x4c00    ; Terminate program
+int  0x21                ; DOS interrupt to exit
