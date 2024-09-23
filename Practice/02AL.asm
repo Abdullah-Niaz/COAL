8c898c66b173eb9b8657bf1d5d2e1cb39e0a4c3e
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-09-23 22:41:44 +0500

    practiced

--- Practice/02AL.asm
+++ Practice/02AL.asm
@@ -1,9 +1,9 @@
-org 0x0100;
+org  0x0100              ;
 
-mov ax,20;
-mov bx,30;
-add ax,bx;
-mov bx,50;
-mov ax,bx;
-mov ax, 0x4c00;
-int 0x21;
+mov  ax,20               ;
+mov  bx,30               ;
+add  ax,bx               ;
+mov  bx,50               ;
+mov  ax,bx               ;
+mov  ax,       0x4c00    ;
+int  0x21                ;
